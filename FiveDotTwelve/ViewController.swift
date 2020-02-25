69ade6d4ce4ec9ae70111ca1359a33357e654c5c
Pawel <paewel@gmail.com>
2020-02-25 16:25:32 +0100

    Initial Commit

---
+++ FiveDotTwelve/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FiveDotTwelve
+//
+//  Created by Pawel on 25/02/2020.
+//  Copyright © 2020 Pawel Jurczyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
